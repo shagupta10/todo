2bc2078ef335bc198a9af4169d1753943d1ac961
unknown <shaguptaf@ULTP_204.synerzipune.local>
2015-01-22 16:11:46 +0530

    initital commit

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+/**
+ * Created by shaguptaf on 22-1-2015.
+ */
+angular.module("todoApp",[])
+    .controller("MainCtrl",function($scope){
+      $scope.todos=[
+          {
+              text: "Todo 1",
+              completed: false
+          },
+          {
+              text: "Todo 2",
+              completed: false
+          }
+      ];
+    $scope.add=function(){
+        $scope.todos.push($scope.newTodo);
+        $scope.newTodo={};
+    }
+        $scope.delete=function(delTodo){
+
+            var index = $scope.todos.indexOf(delTodo)
+            $scope.todos.splice(index, 1);
+
+        }
+        $scope.clearCompleted=function(){
+           for(var index=$scope.todos.length-1; index>=0; index--){
+               if($scope.todos[index].completed===true)
+                   $scope.todos.splice(index, 1);
+           }
+
+        }
+
+    });
