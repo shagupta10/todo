a3a2454245385f1da061efbe551e30cf2e052475
unknown <shaguptaf@ULTP_204.synerzipune.local>
2015-01-23 19:50:07 +0530

    css applied

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
 angular.module("myApp",["todo"]);
+it('should check ng-class-odd and ng-class-even', function() {
+    expect(element(by.repeater('item in todos').row(0).column('item')).getAttribute('class')).
+        toMatch(/odd/);
+    expect(element(by.repeater('item in todos').row(1).column('item')).getAttribute('class')).
+        toMatch(/even/);
+});
